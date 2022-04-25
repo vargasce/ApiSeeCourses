1b9f9655fd59febffe7c8a668b92cd1e03c951e6
vargasce <cris.ema.vargas20@gmail.com>
2022-04-25 11:23:54 -0300

    update

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = props.PORT;
 const host = props.URL;
 const http = require('http');
 
-console.log( node_env );
-
 process.env.DB = props.DB;
 process.env.timeHora = props.updateFinalizadosTime.hora;
 process.env.timeMinutos = props.updateFinalizadosTime.minutos;
